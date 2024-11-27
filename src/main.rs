480be00352199c15774c0300306a593fbfdc06ed
Leo Qi <leo@leozqi.com>
2024-11-27 17:49:52 -0500

    Start project

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // unwrap panics if result is error
+    // let's not do this
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((stream, _)) => {
+                tokio::spawn(handle_connection(stream));
+            }
+            Err(e) => {
+                println!("error: {}", e);
+                continue;
+            }
+        }
+    }
+}
+
+// ownership of closure context of stream passed here.
+async fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    loop {
+        match stream.read(&mut buffer).await {
+            Ok(size) if size != 0 => {
+                let response = "+PONG\r\n";
+
+                if let Err(e) = stream.write_all(response.as_bytes()).await {
+                    eprintln!("error writing to socket: {}", e);
+                }
+            }
+            Ok(_) => {
+                println!("Connection closed");
+                break;
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+                break;
+            }
+        }
+    }
+}
+
